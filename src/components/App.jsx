373d53622df66d6cffa03335190cee36e8a4a5e0
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-09-08 00:42:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,15 @@ import { Filter } from './Filter/Filter';
 import { StyledPhoneBook } from './PhoneBook/PhoneBook.styled';
 import { GlobalStyle } from './GlobalStyle';
 
+const defaultContacts = [
+    { id: 'id-1', Name: 'Rosie Simpson', Number: '459-12-56' },
+    { id: 'id-2', Name: 'Hermione Kline', Number: '443-89-12' },
+    { id: 'id-3', Name: 'Eden Clements', Number: '645-17-79' },
+    { id: 'id-4', Name: 'Annie Copeland', Number: '227-91-26' },
+]
+
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    // { id: 'id-1', Name: 'Rosie Simpson', Number: '459-12-56' },
-    // { id: 'id-2', Name: 'Hermione Kline', Number: '443-89-12' },
-    // { id: 'id-3', Name: 'Eden Clements', Number: '645-17-79' },
-    // { id: 'id-4', Name: 'Annie Copeland', Number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(defaultContacts);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
